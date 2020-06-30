009d2801242de423d8bcac9ac0e8de3f10648433
DESKTOP-MTL7LKS\Clay <claybeaver1991@gmail.com>
2020-06-30 12:24:09 -0500

    Completed HW without bonus. Need to double check work for errors

--- EmployeeSQL/sql_homework.sql
+++ EmployeeSQL/sql_homework.sql
@@ -1,6 +1,6 @@
--- ********************
--- **DATA ENGINEERING**
--- ********************
+-- **************************
+-- **** DATA ENGINEERING ****
+-- **************************
 
 /*
 
@@ -101,10 +101,171 @@ SELECT * FROM titles;
 */
 
 
+-- **********************
+-- ******* TABLES *******
+-- **********************
 
 
+-- TABLES 1-6                  #
+SELECT * FROM departments; --- 1
+SELECT * FROM dept_emp; ------ 2
+SELECT * FROM dept_manager; -- 3
+SELECT * FROM employees; ----- 4
+SELECT * FROM salaries; ------ 5
+SELECT * FROM titles; -------- 6
+-- NEW TABLES 7-10                           #
+SELECT * FROM employee_salary; ------------- 7 --- Data Analysis #1
+SELECT * FROM eightysix_employees; --------- 8 --- Data Analysis #2
+SELECT * FROM manager_department; ---------- 9
+SELECT * FROM manager_info; ---------------- 10 -- Data Analysis #3
+SELECT * FROM employee_department_number; -- 11
+SELECT * FROM employee_department_name; ---- 12
+SELECT * FROM employee_department_info; ---- 13 -- Data Analysis #4
+SELECT * FROM employee_hercules_b; --------- 14 -- Data Analysis #5
+SELECT * FROM employee_sales_dept; --------- 15 -- Data Analysis #6
+SELECT * FROM employee_salesdev_dept; ------ 16 -- Data Analysis #7
+SELECT * FROM last_name_count; ------------- 17 -- Data Analysis #8
 
--- ********************
--- ****DATA ANALYSIS***
--- ********************
 
+-- ***********************
+-- **** DATA ANALYSIS ****
+-- ***********************
+
+
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+-- Merge tables 4.employees and 5.salaries into new table 7.employee_salary
+
+DROP TABLE IF EXISTS employee_salary;
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+INTO employee_salary
+FROM salaries
+INNER JOIN employees ON
+employees.emp_no=salaries.emp_no;
+
+SELECT * FROM employee_salary;
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+-- Take select information from table 4.employees into new table 8.eightysix_employees
+
+DROP TABLE IF EXISTS eightysix_employees;
+SELECT employees.first_name, employees.last_name, employees.hire_date
+INTO eightysix_employees
+FROM employees
+WHERE
+	hire_date >= DATE '1986-01-01'
+	AND hire_date <= DATE '1986-12-31';
+	
+SELECT * FROM eightysix_employees;
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+-- First, merge table 1.departments into table 3.dept_manager to create table 9.manager_department
+
+DROP TABLE IF EXISTS manager_department;
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no
+INTO manager_department
+FROM dept_manager
+INNER JOIN departments ON
+dept_manager.dept_no=departments.dept_no;
+
+SELECT * FROM manager_department;
+
+-- Second, merge table 9.manager_department into table 4.employees to create table 10.manager_info
+
+DROP TABLE IF EXISTS manager_info;
+SELECT manager_department.dept_no, manager_department.dept_name, employees.emp_no, employees.last_name, employees.first_name
+INTO manager_info
+FROM manager_department
+INNER JOIN employees ON
+manager_department.emp_no=employees.emp_no;
+
+SELECT * FROM manager_info;
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- First, merge table 2.dept_emp with table 4.employees to match emp_no to dept_no and create table 11.employee_department_number
+
+DROP TABLE IF EXISTS employee_department_number;
+SELECT employees.emp_no, dept_emp.dept_no
+INTO employee_department_number
+FROM dept_emp
+INNER JOIN employees ON
+dept_emp.emp_no=employees.emp_no;
+
+SELECT * FROM employee_department_number;
+
+-- Second, merge table 11.employee_department_number with table 1.departments to link employeer number with dapartment name and create table 12.employee_department_name
+
+DROP TABLE IF EXISTS employee_department_name;
+SELECT employee_department_number.emp_no, departments.dept_name
+INTO employee_department_name
+FROM employee_department_number
+INNER JOIN departments ON
+employee_department_number.dept_no=departments.dept_no;
+
+SELECT * FROM employee_department_name;
+
+-- Third, merge table 12.employee_department_name with table 4.employees to create the final table 13.employee_department_info
+
+DROP TABLE IF EXISTS employee_department_info;
+SELECT employee_department_name.emp_no, employees.last_name, employees.first_name, employee_department_name.dept_name
+INTO employee_department_info
+FROM employees
+INNER JOIN employee_department_name ON
+employee_department_name.emp_no=employees.emp_no;
+
+SELECT * FROM employee_department_info;
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+-- Create new table 14.employee_hercules_b to display results of analysis.
+
+DROP TABLE IF EXISTS employee_hercules_b;
+SELECT *
+INTO employee_hercules_b
+FROM employees
+WHERE first_name LIKE 'Hercules'
+	AND last_name LIKE 'B%';
+	
+SELECT * FROM employee_hercules_b;
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+-- Create new table 15.employee_sales_dept to display results of analysis.
+
+DROP TABLE IF EXISTS employee_sales_dept;
+SELECT *
+INTO employee_sales_dept
+FROM employee_department_info
+WHERE dept_name LIKE 'Sales';
+	
+SELECT * FROM employee_sales_dept;
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- Create new table 16.employee_salesdev_dept to display results of analysis.
+
+DROP TABLE IF EXISTS employee_salesdev_dept;
+SELECT *
+INTO employee_salesdev_dept
+FROM employee_department_info
+WHERE dept_name LIKE 'Sales'
+	OR dept_name LIKE 'Development';
+	
+SELECT * FROM employee_salesdev_dept;
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- Create new table 17.last_name_count to display results of analysis.
+
+DROP TABLE IF EXISTS last_name_count;
+SELECT last_name, COUNT(last_name)AS Frequency
+INTO last_name_count
+FROM employees
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
+
+SELECT * FROM last_name_count;
+
+/*
+SELECT last_name, COUNT(last_name)AS Frequency
+FROM employee_department_info
+GROUP BY last_name                                   -- Note: When I ran from employee_department_info I got a different result?
+ORDER BY
+COUNT(last_name) DESC;
+*/
